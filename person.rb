e825e9d118f3ca81c6d7ccc742a6a7cf138f6107
Chronos <sylvain.santucci@gmail.com>
2012-04-06 13:40:55 +0200

    Premier Commit

---
+++ person.rb
@@ -0,0 +1,28 @@
+class Person
+
+  def initialize(un_nom, un_prenom)
+    @nom = un_nom
+    @prenom = un_prenom
+  end
+
+  #getter
+  def nom
+    return @nom
+  end
+
+  #setter
+  def nom=(un_nom)
+    @nom=un_nom
+  end
+
+  #getter
+  def prenom
+    return @prenom
+  end
+
+  #setter
+  def prenom=(un_prenom)
+    @prenom=un_prenom
+  end
+
+end
